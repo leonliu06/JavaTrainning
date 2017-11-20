cfab8f30417462a3e7a30d7a8d5fb680e2a2769c
leonliu06 <leonliu06@gmail.com>
2017-11-20 18:15:38 +0800

    exception

---
+++ FullConstructors.java
@@ -0,0 +1,29 @@
+// FullConstructors.java
+
+class MyException extends Exception{
+    public MyException(){}
+    public MyException(String msg){ super(msg); }
+}
+
+public class FullConstructors{
+    public static void f() throws MyException{
+        System.out.println("Throwing MyException form f()");
+        throw new MyException();
+    }
+    public static void g() throws MyException{
+        System.out.println("Throwing MyException form g()");
+        throw new MyException("Originated in g()");
+    }
+    public static void main(String[] args){
+        try{
+            f();
+        }catch(MyException e){
+            e.printStackTrace(System.err);
+        }
+        try{
+            g();
+        }catch(MyException e){
+            e.printStackTrace(System.err);
+        }
+    }
+}
